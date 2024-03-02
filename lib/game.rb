5392a00eece81385620def961013af4c5b349c80
Uzawa Ren <96781771+uzawa-ren@users.noreply.github.com>
2024-03-02 13:34:56 +0200

    Add playing and turns scripts

--- lib/game.rb
+++ lib/game.rb
@@ -15,4 +15,47 @@ class Game
   def update_winner(color)
     @winner = color
   end
+
+  def play
+    instruduction
+    cage.show
+    turns
+    conclusion
+  end
+
+  def turns
+    until cage.game_over?
+      put_user_piece
+      put_computer_piece
+      cage.show
+      break if cage.full?
+    end
+  end
+
+  private
+
+  def instruduction
+    puts 'Welcome to the game Connect Four!'
+    puts 'To win, you need to put your pieces in a horizontal, vertical'
+    puts 'or diagonal 4-in-row. Your pieces are yellow. Good luck!'
+  end
+
+  def put_user_piece
+    column_index = user_input
+    cage.put_piece(column_index, '33')
+  end
+
+  def put_computer_piece
+    column_index = rand(0..6)
+    cage.put_piece(column_index, '34')
+  end
+
+  def conclusion
+    if winner
+      winner = @winner == '33' ? 'Yellow' : 'Blue'
+      puts "Gameover! #{winner} wins!"
+    elsif cage.full?
+      puts 'A draw!'
+    end
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -15,6 +15,27 @@ describe Game do
       expect { game.update_winner(new_winner_color) }.to change { game.winner }.to(new_winner_color)
     end
   end
+
+  describe '#turns' do
+    context 'when game is ended in 4 turns' do
+      before do
+        allow(game.cage).to receive(:game_over?).and_return(false, false, false, false, true)
+        allow(game.cage).to receive(:show)
+        allow(game).to receive(:put_user_piece)
+        allow(game).to receive(:put_computer_piece)
+      end
+
+      it 'sends put_user_piece 4 times' do
+        expect(game).to receive(:put_user_piece).exactly(4).times
+        game.turns
+      end
+
+      it 'sends put_computer_piece 4 times' do
+        expect(game).to receive(:put_computer_piece).exactly(4).times
+        game.turns
+      end
+    end
+  end
 end
 
 # rubocop:enable Metrics/BlockLength
